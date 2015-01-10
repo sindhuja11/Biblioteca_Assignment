a4363e4ccf2fe86c9641adada0fd9d164d97ff0a
Administrator <Administrator@INb.local>
2015-01-10 10:50:00 +0530

    First Commit

---
+++ test/com/twu/biblioteca/bibliotecaTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class bibliotecaTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+
+}
