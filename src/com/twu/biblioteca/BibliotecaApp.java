44cce6fe8cb73c2b2b5f4335f08e7c371502baa5
Administrator <Administrator@INb.local>
2015-01-10 18:49:16 +0530

    first commit till list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,37 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BibliotecaApp {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+
+    }
+
+
+    public void CheckWelcomeMessage(String message) {
+        System.out.print(message);
     }
 
 
 
 
+    public void CheckDisplayOfBookList() {
+        String line = "";
+        try {
+            FileReader fr = new FileReader("/Users/Administrator/Downloads/TWU_Biblioteca-master/src/com/twu/biblioteca/booklist");
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- test/com/twu/biblioteca/bibliotecaTest.java
+++ test/com/twu/biblioteca/bibliotecaTest.java
@@ -1,15 +1,52 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class bibliotecaTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
     public void test() {
         assertEquals(1, 1);
     }
+    @Test
+    public void ShouldCheckWelcomeMessage() {
+        new BibliotecaApp().CheckWelcomeMessage("hello!Welcome to Biblioteca");
+        assertEquals("hello!Welcome to Biblioteca", outContent.toString());
+    }
+    @Test
+    public void ShouldCheckListOfBooks() {
+        new BibliotecaApp().CheckDisplayOfBookList();
+        assertEquals("Digital Fortress\n" +
+                "Java The Complete Reference\n" +
+                "Software Engineering\n", outContent.toString());
+           }
+
 
 
 }
